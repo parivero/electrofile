a6868dc0ce52520ced7d8ccb68c33c7804fe2094
parivero <cocoriv@gmail.com>
2018-06-18 16:28:27 -0300

    Drop Zone

--- src/renderer/components/ServerCard/FileUpload/FileUpload.vue
+++ src/renderer/components/ServerCard/FileUpload/FileUpload.vue
@@ -16,97 +16,115 @@
 </template>
 
 <script>
+import scp from "scp2";
 
-const STATUS_INITIAL = 0, STATUS_SAVING = 1, STATUS_SUCCESS = 2, STATUS_FAILED = 3;
+const STATUS_INITIAL = 0,
+  STATUS_SAVING = 1,
+  STATUS_SUCCESS = 2,
+  STATUS_FAILED = 3;
+
+const client = new scp.Client();
 
 export default {
-    data() {
-      return {
-        uploadedFiles: [],
-        uploadError: null,
-        currentStatus: null,
-        uploadFieldName: 'file'
-      }
+  data() {
+    return {
+      uploadedFiles: [],
+      uploadError: null,
+      currentStatus: null,
+      uploadFieldName: "file"
+    };
+  },
+  created() {
+    this.createClient();
+  },
+  computed: {
+    isInitial() {
+      return this.currentStatus === STATUS_INITIAL;
     },
-    computed: {
-      isInitial() {
-        return this.currentStatus === STATUS_INITIAL;
-      },
-      isSaving() {
-        return this.currentStatus === STATUS_SAVING;
-      },
-      isSuccess() {
-        return this.currentStatus === STATUS_SUCCESS;
-      },
-      isFailed() {
-        return this.currentStatus === STATUS_FAILED;
-      }
+    isSaving() {
+      return this.currentStatus === STATUS_SAVING;
     },
-    methods: {
-      reset() {
-        // reset form to initial state
-        this.currentStatus = STATUS_INITIAL;
-        this.uploadedFiles = [];
-        this.uploadError = null;
-      },
-      save(formData) {
-        // upload data to the server
-        this.currentStatus = STATUS_SAVING;
-
-        console.log(formData);
-      },
-      filesChange(fieldName, fileList) {
-        // handle file changes
-        const formData = new FormData();
-
-        if (!fileList.length) return;
-
-        // append the files to FormData
-        Array
-          .from(Array(fileList.length).keys())
-          .map(x => {
-            console.log(fileList[x].path);
-            formData.append(fieldName, fileList[x], fileList[x].name);
-          });
-
-        // save it
-        this.save(formData);
-      }
+    isSuccess() {
+      return this.currentStatus === STATUS_SUCCESS;
     },
-    mounted() {
-      this.reset();
+    isFailed() {
+      return this.currentStatus === STATUS_FAILED;
+    }
+  },
+  methods: {
+    createClient() {
+      client.on("transfer", function(buf, uploading, total) {
+        console.log("uploading: " + uploading);
+        console.log("total: " + total);
+      });
     },
+    reset() {
+      // reset form to initial state
+      this.currentStatus = STATUS_INITIAL;
+      this.uploadedFiles = [];
+      this.uploadError = null;
+    },
+    save(formData) {
+      // upload data to the server
+      this.currentStatus = STATUS_SUCCESS;
+
+      this.currentStatus = STATUS_SAVING;
+
+      console.log(formData);
+    },
+    filesChange(fieldName, fileList) {
+      // handle file changes
+      const formData = new FormData();
+
+      if (!fileList.length) return;
+
+      scp.scp(
+        fileList[0].path,
+        "pntdesa:RPJ82YMaMiFFTt9RgVhp@dev.somospnt.com:/home/pntdesa",
+        client,
+        function(err) {
+          if (err) {
+            console.log(err);
+          }
+          // save it
+          this.save(formData);
+        }
+      );
+    }
+  },
+  mounted() {
+    this.reset();
   }
-
+};
 </script>
 
 <style scoped>
-  .dropbox {
-    outline: 2px dashed grey; /* the dash box */
-    outline-offset: -10px;
-    background: dimgray;
-    color: black;
-    padding: 10px 10px;
-    min-height: 50px; /* minimum height */
-    position: relative;
-    cursor: pointer;
-  }
+.dropbox {
+  outline: 2px dashed grey; /* the dash box */
+  outline-offset: -10px;
+  background: dimgray;
+  color: black;
+  padding: 10px 10px;
+  min-height: 50px; /* minimum height */
+  position: relative;
+  cursor: pointer;
+}
 
-  .input-file {
-    opacity: 0; /* invisible but it's there! */
-    width: 50%;
-    height: 50px;
-    position: absolute;
-    cursor: pointer;
-  }
+.input-file {
+  opacity: 0; /* invisible but it's there! */
+  width: 50%;
+  height: 50px;
+  position: absolute;
+  cursor: pointer;
+}
 
-  .dropbox:hover {
-    background: lightblue; /* when mouse over to the drop zone, change color */
-  }
+.dropbox:hover {
+  background: lightblue; /* when mouse over to the drop zone, change color */
+}
 
-  .dropbox p {
-    font-size: 1.2em;
-    text-align: center;
-    padding: 50px 0;
-  }
+.dropbox p {
+  font-size: 1.2em;
+  text-align: center;
+  padding: 50px 0;
+}
 </style>
